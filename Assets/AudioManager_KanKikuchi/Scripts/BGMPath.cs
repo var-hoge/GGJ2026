1f60e3f30a4aa266ec74c69ad70ccf19cd646075
ほげ <fictives73@gmail.com>
2026-02-01 14:06:25 +0900

    オーディオ追加

--- Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -5,6 +5,8 @@
 /// </summary>
 public static class BGMPath{
 
+	public const string AUDIO_CUTSCENE  = "BGM/Audio_Cutscene";
+	public const string AUDIO_ENDING    = "BGM/Audio_Ending";
 	public const string BATTLE27        = "BGM/Battle/battle27";
 	public const string FANTASY14       = "BGM/fantasy14";
 	public const string HEARTBEAT01     = "BGM/heartbeat01";
