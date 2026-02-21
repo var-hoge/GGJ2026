09f64ea421b39bac33f7f532a761e7c9cb5d7cde
ほげ <fictives73@gmail.com>
2026-02-21 15:03:23 +0900

    ねこの生成

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using IsoTools.Physics;
+using System.Collections;
+
+namespace IsoTools.Examples.Kenney {
+	[RequireComponent(typeof(IsoRigidbody))]
+	public class Cat : MonoBehaviour {
+
+		IsoObject    _isoObject    = null;
+		IsoRigidbody _isoRigidbody = null;
+
+		[SerializeField] private Transform _spriteObj = null;
+		
+		void Start() {
+			_isoObject = GetComponent<IsoObject>();
+			if ( !_isoObject ) {
+				throw new UnityException("AlienBallController. IsoObject component not found!");
+			}
+			_isoRigidbody = GetComponent<IsoRigidbody>();
+			if ( !_isoRigidbody ) {
+				throw new UnityException("AlienBallController. IsoRigidbody component not found!");
+			}
+			StartCoroutine(AddRndForce());
+		}
+
+		void Update() {
+			if ( _isoObject.positionZ < 0.0f ) {
+				Destroy(gameObject);
+			}
+		}
+
+		IEnumerator AddRndForce() {
+			while ( true ) {
+				var dx = Random.Range(-2.0f, 2.0f);
+				var dy = Random.Range(-2.0f, 2.0f);
+				_isoRigidbody.AddForce(new Vector3(dx, dy, 0.0f), ForceMode.Impulse);
+
+				// 移動方向に応じてスプライトの向きを変更
+				var sign = Mathf.Sign(dx);
+				var scale = _spriteObj.localScale;
+				_spriteObj.localScale = new Vector3(scale.x * sign, scale.y, scale.z);
+
+				yield return new WaitForSeconds(1);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/CatSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace IsoTools.Examples.Kenney {
+	public class CatSpawner : MonoBehaviour {
+
+		[SerializeField] private int maxCatCount   = 1;
+		[SerializeField] private GameObject catPrefab = null;
+		private IsoWorld iso_world = null;
+		
+		void Start() {
+			if ( !catPrefab ) {
+				throw new UnityException("CatSpawner. Cat prefab not found!");
+			}
+			iso_world = IsoWorld.GetWorld(0);
+			StartCoroutine(SpawnCats());
+
+			for (var n = 0; n < maxCatCount; ++n)
+			{
+				Spawn();
+			}
+		}
+		
+		IEnumerator SpawnCats() {
+			while ( true ) {
+				var cats = GameObject.FindObjectsOfType<Cat>();
+				if (cats.Length < maxCatCount)
+				{
+					Spawn();
+				}
+				yield return new WaitForSeconds(Random.Range(2.0f, 5.0f));
+			}
+		}
+
+		void Spawn()
+		{
+			var dx = Random.Range(-5.0f, 5.0f);
+			var dy = Random.Range(-5.0f, 5.0f);
+			var catObj = Instantiate(catPrefab, iso_world.transform);
+			var cat_iso_obj = catObj.GetComponent<IsoObject>();
+			cat_iso_obj.position = new Vector3(dx, dy, 0.5f);
+		}
+	}
+}
