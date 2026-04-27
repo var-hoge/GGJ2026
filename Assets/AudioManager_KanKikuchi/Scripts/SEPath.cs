62617ff325565e2cf6ceb9854fdea72f664fd872
Cornelia Z <124412524+LeConSound@users.noreply.github.com>
2026-04-27 21:52:51 +0900

    SFX追加

--- Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -5,16 +5,20 @@
 /// </summary>
 public static class SEPath{
 
-	public const string AUDIO_ENDING     = "SE/Audio_Ending";
-	public const string JINGLE10         = "SE/Jingle/jingle10";
-	public const string SD_GAME_LOOP     = "SE/SD_Game_Loop";
-	public const string SFX_GAME_CORRECT = "SE/SFX_Game_Correct";
-	public const string SFX_GAME_WRONG_1 = "SE/SFX_Game_Wrong_1";
-	public const string SFX_GAME_WRONG_2 = "SE/SFX_Game_Wrong_2";
-	public const string SFX_GAME_WRONG_3 = "SE/SFX_Game_Wrong_3";
-	public const string SYSTEM20         = "SE/system20";
-	public const string SYSTEM23         = "SE/system23";
-	public const string UI_SELECT        = "SE/UI_Select";
+	public const string AUDIO_ENDING         = "SE/Audio_Ending";
+	public const string JINGLE10             = "SE/Jingle/jingle10";
+	public const string SD_GAME_LOOP         = "SE/SD_Game_Loop";
+	public const string SFX_CROWD_CHEERING   = "SE/SFX_CrowdCheering";
+	public const string SFX_GAME_CORRECT     = "SE/SFX_Game_Correct";
+	public const string SFX_GAME_WRONG_1     = "SE/SFX_Game_Wrong_1";
+	public const string SFX_GAME_WRONG_2     = "SE/SFX_Game_Wrong_2";
+	public const string SFX_GAME_WRONG_3     = "SE/SFX_Game_Wrong_3";
+	public const string SFX_HERO_TWINKLE     = "SE/SFX_HeroTwinkle";
+	public const string SFX_POLICE_DOG_READY = "SE/SFX_PoliceDogReady";
+	public const string SFX_VAULT_DOOR       = "SE/SFX_VaultDoor";
+	public const string SYSTEM20             = "SE/system20";
+	public const string SYSTEM23             = "SE/system23";
+	public const string UI_SELECT            = "SE/UI_Select";
 
 }
 
