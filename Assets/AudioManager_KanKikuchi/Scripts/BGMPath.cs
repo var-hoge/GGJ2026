5711b59be8519240989946fb79cdc5d14fb39c16
Cornelia Z <124412524+LeConSound@users.noreply.github.com>
2026-05-01 01:23:44 +0900

    Added CS SFX System

--- Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -5,14 +5,15 @@
 /// </summary>
 public static class BGMPath{
 
-	public const string AUDIO_CUTSCENE    = "BGM/Audio_Cutscene";
-	public const string BATTLE27          = "BGM/Battle/battle27";
-	public const string FANTASY14         = "BGM/fantasy14";
-	public const string HEARTBEAT01       = "BGM/heartbeat01";
-	public const string MUSIC_ENDING_LOOP = "BGM/Music_Ending_Loop";
-	public const string MUSIC_GAME_LOOP   = "BGM/Music_Game_Loop";
-	public const string MUSIC_MENU_LOOP   = "BGM/Music_Menu_Loop";
-	public const string MUSIC_MENU_LOOP1  = "BGM/Music_Menu_Loop 1";
+	public const string AUDIO_CUTSCENE      = "BGM/Audio_Cutscene";
+	public const string BATTLE27            = "BGM/Battle/battle27";
+	public const string FANTASY14           = "BGM/fantasy14";
+	public const string HEARTBEAT01         = "BGM/heartbeat01";
+	public const string MUSIC_CUTSCENE_LOOP = "BGM/Music_Cutscene_Loop";
+	public const string MUSIC_ENDING_LOOP   = "BGM/Music_Ending_Loop";
+	public const string MUSIC_GAME_LOOP     = "BGM/Music_Game_Loop";
+	public const string MUSIC_MENU_LOOP     = "BGM/Music_Menu_Loop";
+	public const string MUSIC_MENU_LOOP1    = "BGM/Music_Menu_Loop 1";
 
 }
 
